1143d74869d8510cf471436af2287b021d98ce59
Hoang Minh <hoangminh30042003@gmail.com>
2024-10-01 14:27:40 +0700

    fix path

--- dist/assets/index-BSmETVs_.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CMMdL1Hv.js";const r=()=>t.jsx(t.Fragment,{});export{r as default};
--- dist/assets/index-hKuIhjyu.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CMMdL1Hv.js";const o=()=>t.jsx(t.Fragment,{});export{o as default};
