58b7c9703c0fc2cc6b5f9029c060490d171f05b4
Hoang Minh <hoangminh30042003@gmail.com>
2024-10-03 17:37:53 +0700

    fix path header

--- src/app/component/header/index.tsx
+++ src/app/component/header/index.tsx
@@ -54,7 +54,7 @@ const HeaderBar: React.FC = () => {
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
-            href="https://hmng3004.github.io/HoangMinh/#/"
+            href="https://hoang-minh.vercel.app/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
